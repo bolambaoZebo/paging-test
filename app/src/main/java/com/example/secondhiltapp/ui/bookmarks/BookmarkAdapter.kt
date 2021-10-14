e68a6c90b8ff4c59d00a13b534d257aa87afd903
96group <citzebedee@gmail.com>
2021-10-14 17:36:03 +0800

    updated home, highlights, continue with bookmarks

--- app/src/main/java/com/example/secondhiltapp/ui/bookmarks/BookmarkAdapter.kt
+++ app/src/main/java/com/example/secondhiltapp/ui/bookmarks/BookmarkAdapter.kt
@@ -1,9 +1,43 @@
 package com.example.secondhiltapp.ui.bookmarks
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.secondhiltapp.databinding.ItemBookmarkBinding
 import com.example.secondhiltapp.db.entity.BookMarkData
 
-class BookmarkAdapter : ListAdapter<BookMarkData, > {
+class BookmarkAdapter : ListAdapter<BookMarkData, BookmarkAdapter.BookMarkViewHolder>(DiffCallback()) {
 
-    class
+    class BookMarkViewHolder(private val binding: ItemBookmarkBinding) : RecyclerView.ViewHolder(binding.root){
+
+        fun bind(bookmark: BookMarkData){
+            binding.apply {
+                bookmarkItemTitle.text = bookmark.title
+                bookmarkItemDescription.text = bookmark.description
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookMarkViewHolder {
+        val binding = ItemBookmarkBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return BookMarkViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BookMarkViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<BookMarkData>(){
+        override fun areItemsTheSame(oldItem: BookMarkData, newItem: BookMarkData) =
+            oldItem.uid == newItem.uid
+
+        override fun areContentsTheSame(oldItem: BookMarkData, newItem: BookMarkData) =
+            oldItem == newItem
+
+    }
 }
